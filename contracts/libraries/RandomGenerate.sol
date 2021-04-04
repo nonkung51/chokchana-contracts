fba2b6539ee45fb10b64a3e3a27e6fb1684cbd80
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-04 23:40:36 +0700

    :sparkles: add seed to randomGen

--- contracts/libraries/RandomGenerate.sol
+++ contracts/libraries/RandomGenerate.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.3;
 
 library RandomGenerate {
-    function randomGen(uint256 max) internal view returns (uint256 randomNumber) {
-        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp))) % max;
+    function randomGen(uint256 max, uint256 seed) internal view returns (uint256 randomNumber) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, seed))) % max;
     }
 }
