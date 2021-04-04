0b07bf08b3a80bdc6b7aa372f69ec1a5d21e070c
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-04 16:12:05 +0700

    :sparkles: add Ownable

--- contracts/chokchanaTicket.sol
+++ contracts/chokchanaTicket.sol
@@ -4,18 +4,36 @@ pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract ChokchanaTicket is ERC721, ERC721Enumerable {
-    uint256[] public numbers;
+contract ChokchanaTicket is ERC721, ERC721Enumerable, Ownable {
+    uint256 curId;
+    mapping(uint256 => uint256) public numbers;
+    mapping(uint256 => uint) public issuesDate;
+    mapping(uint256 => bool) public exists;
+    bool public multiple;
     
-    constructor() ERC721("Chokchana Ticket", "CCNT") {
-        
+    constructor(bool _multiple) ERC721("Chokchana Ticket", "CCNT") Ownable() {
+        curId = 0;
+        multiple = _multiple;
     }
     
     function mint(uint256 number) public {
-        numbers.push(number);
-        _safeMint(msg.sender, numbers.length - 1);
+        if (!multiple && exists[number]) {
+            revert("Can only mint 1 ticket of same number!");
+        }
+        numbers[curId] = number;
+        issuesDate[curId] = block.timestamp;
+        exists[number] = true;
+        
+        // Mint and send to msg.sender
+        _safeMint(msg.sender, curId);
+        curId += 1;
+    }
+    
+    function get(uint256 idx) public view returns(uint256, uint) {
+        return (numbers[idx], issuesDate[idx]);
     }
     
     /* ERC721Enumerable require to override */
