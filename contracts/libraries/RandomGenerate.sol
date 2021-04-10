f6a51241aaaed7cf60b81debeb3d563b907b2b3f
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-10 11:02:38 +0700

    :white_check_mark: add test for claim reward

--- contracts/libraries/RandomGenerate.sol
+++ contracts/libraries/RandomGenerate.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.3;
+import "hardhat/console.sol";
 
 library RandomGenerate {
     function randomGen(uint256 max, uint256 seed) internal view returns (uint256 randomNumber) {
-        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, seed))) % max;
+        uint256 rand = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, seed))) % max;
+        console.log('rand num: ', rand);
+        return rand;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
+// require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,9 +19,9 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: "0.8.3",
-  gasReporter: {
-    currency: 'BNB',
-    gasPrice: 5
-  }
+  // gasReporter: {
+  //   currency: 'BNB',
+  //   gasPrice: 5
+  // }
 };
 
