417705ec200178986f9ecc07ba61900056eb40ef
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-11 14:40:03 +0700

    :sparkles: add BoxV2.sol

--- contracts/upgradable/BoxV2.sol
+++ contracts/upgradable/BoxV2.sol
@@ -22,4 +22,10 @@ contract Box is Initializable {
     function retrieve() public view returns (uint256) {
         return value;
     }
+
+    // Increments the stored value by 1
+    function increment() public {
+        value = value + 1;
+        emit ValueChanged(value);
+    }
 }
--- scripts/create-box.js
+++ scripts/create-box.js
@@ -3,6 +3,7 @@ const { ethers, upgrades } = require('hardhat');
 
 async function main() {
 	const Box = await ethers.getContractFactory('Box');
+	console.log('Deploying Box...');
 	const box = await upgrades.deployProxy(Box, [42]);
 	await box.deployed();
 	console.log('Box deployed to:', box.address);
