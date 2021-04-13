764da09f8563c762082846670bc2fd7c753eac0a
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-13 12:03:59 +0700

    :memo: add comments on ChokchanaTicket.sol

--- contracts/chokchanaTicket.sol
+++ contracts/chokchanaTicket.sol
@@ -2,25 +2,38 @@
 
 pragma solidity ^0.8.3;
 
+// ERC721 -> NFT
+// ERC721Enumerable -> making NFT queriable
+// Ownable for permission control of contract
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract ChokchanaTicket is ERC721, ERC721Enumerable, Ownable {
+    // curId keep track of last id of NFT generated
     uint256 curId;
+    // current round of reward pools
     uint256 curRound;
+    // round of each ticket (map with id)
     mapping(uint256 => uint256) public rounds;
+    // mapping of round and id to ticket's number
     mapping(uint256 => mapping(uint256 => uint256)) public numbers;
+    // numbers of tickets of each number in each round
     mapping(uint256 => mapping(uint256 => uint256)) public numOfNumbers;
+    // whether the numbers in that round is exists
     mapping(uint256 => mapping(uint256 => bool)) public exists;
+    // is that ticket id is claimed
     mapping(uint256 => bool) public claimed;
+    // is the same number can be mint more than one time
     bool public multiple;
     
+    // range of ticket numbers
     uint256 startNumber;
     uint256 endNumber;
     
     constructor(bool _multiple, uint256 _startNumber, uint256 _endNumber) ERC721("Chokchana Ticket", "CCNT") Ownable() {
+        // Initialize everything
         curId = 0;
         multiple = _multiple;
         curRound = 1;
@@ -29,6 +42,7 @@ contract ChokchanaTicket is ERC721, ERC721Enumerable, Ownable {
         endNumber = _endNumber;
     }
     
+    // return range of tickets number
     function range() public view returns(uint256, uint256) {
         return (startNumber, endNumber);
     }
@@ -38,19 +52,24 @@ contract ChokchanaTicket is ERC721, ERC721Enumerable, Ownable {
         curRound += 1;
     }
 
+    // set ticket claim status to claimed
     function setClaim(uint256 id) public /*onlyOwner*/ {
         claimed[id] = true;
     }
     
+    // Minting new tickets
     function mint(uint256 number, address to) public /*onlyOwner*/ {
+        // check if ticket can be minted or not
         if (!multiple && exists[curRound][number]) {
             revert("Can only mint 1 ticket of same number!");
         }
 
+        // check the range of tickets
         if (number < startNumber || number > endNumber) {
             revert("Can only mint ticket in range of startNumber -> endNumber");
         }
         
+        // set variable to keep track of ticket's info
         numbers[curRound][curId] = number;
         exists[curRound][number] = true;
         
@@ -64,13 +83,16 @@ contract ChokchanaTicket is ERC721, ERC721Enumerable, Ownable {
         
         // Mint and send to msg.sender
         _safeMint(to, curId);
+        // Increment id for next ticket
         curId += 1;
     }
     
+    // get info of each ticket
     function get(uint256 id) public view returns(uint256, uint256, bool) {
         return (numbers[rounds[id]][id], rounds[id], claimed[id]);
     }
     
+    // get number of ticket of that ticket numbers minted
     function getNumberOf(uint256 round, uint256 ticketNumber) public view returns (uint256) {
         return numOfNumbers[round][ticketNumber];
     }
