dd30d315e89ac628afea09d70d3928a4a273fdf0
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-28 14:43:20 +0700

    :alembic: add playground to trying function of contract

---
+++ scripts/playground.js
@@ -0,0 +1,55 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+
+async function main() {
+	console.log('Start deploying process...');
+	const [owner] = await ethers.getSigners();
+	console.log('Deployer is', owner.address);
+
+	const _thbToken = await ethers.getContractFactory('THBToken');
+	const thbToken = await _thbToken.deploy();
+	await thbToken.mint(100000);
+	console.log('Deploy Token contract at: ', thbToken.address);
+
+	const _ticket = await ethers.getContractFactory('ChokchanaTicket');
+	const ticket = await _ticket.deploy(true, 0, 9999);
+	console.log('Deploy Ticket contract at: ', ticket.address);
+
+	const _lottery = await ethers.getContractFactory('ExternalLottery');
+	const lottery = await _lottery.deploy(
+		ticket.address,
+		thbToken.address,
+		80,
+		60,
+		120
+	);
+	console.log('Deploy Lottery contract at: ', lottery.address);
+
+	const _randomGenerator = await ethers.getContractFactory(
+		'RandomGenerator'
+	);
+	const randomGenerator = await _randomGenerator.deploy(
+		lottery.address,
+		ticket.address,
+		3
+	);
+	console.log('Deploy RandomGenerator contract at: ', randomGenerator.address);
+
+    await thbToken.approve(lottery.address, 1000000);
+
+    await lottery.setlockBeforeDraw(60);
+    await lottery.setNextDraw(120);
+    await lottery.buyTicket(2000);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
