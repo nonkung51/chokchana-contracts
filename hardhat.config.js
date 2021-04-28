a24d5d970d639d3cf3db6c1254a07f8d1a3cb714
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2021-04-28 11:28:55 +0700

    :rocket: add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
-// require("hardhat-gas-reporter");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.3",
-  // gasReporter: {
-  //   currency: 'BNB',
-  //   gasPrice: 5
-  // }
+	solidity: '0.8.3',
+	networks: {
+		kovan: {
+			url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+			accounts: [`0x${KOVAN_PRIVATE_KEY}`],
+		},
+	},
 };
-
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,30 +3,40 @@
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require('hardhat');
 
 async function main() {
-	// We get the contract to deploy
-	const THBTokenContract = await hre.ethers.getContractFactory('THBToken');
-	const THBToken = await THBTokenContract.deploy();
+	console.log('Start deploying process...');
+	const [owner] = await ethers.getSigners();
+	console.log('Deployer is', owner.address);
 
-	await THBToken.deployed();
+	const _thbToken = await ethers.getContractFactory('THBToken');
+	const thbToken = await _thbToken.deploy();
+	await thbToken.mint(100000);
+	console.log('Deploy Token contract at: ', thbToken.address);
 
-	console.log('THBToken deployed to:', THBToken.address);
+	const _ticket = await ethers.getContractFactory('ChokchanaTicket');
+	const ticket = await _ticket.deploy(true, 1000, 9999);
+	console.log('Deploy Ticket contract at: ', ticket.address);
 
-	const TicketContract = await hre.ethers.getContractFactory('ChokchanaTicket');
-	const Ticket = await TicketContract.deploy();
+	const _lottery = await ethers.getContractFactory('ExternalLottery');
+	const lottery = await _lottery.deploy(
+		ticket.address,
+		thbToken.address,
+		80,
+		0,
+		0
+	);
+	console.log('Deploy Lottery contract at: ', lottery.address);
 
-	await Ticket.deployed(true, 0, 99);
-
-	console.log('Ticket deployed to:', Ticket.address);
-
-	const LotteryContract = await hre.ethers.getContractFactory('ChokchanaLottery');
-	const Lottery = await LotteryContract.deploy();
-
-	await Lottery.deployed(true, 0, 99);
-
-	console.log('Lottery deployed to:', Lottery.address);
+	const _randomGenerator = await ethers.getContractFactory(
+		'RandomGenerator'
+	);
+	const randomGenerator = await _randomGenerator.deploy(
+		lottery.address,
+		ticket.address,
+		3
+	);
+	console.log('Deploy RandomGenerator contract at: ', randomGenerator.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -37,24 +47,3 @@ main()
 		console.error(error);
 		process.exit(1);
 	});
-
-/*
-const _thbToken = await ethers.getContractFactory('THBToken')
-const thbToken = await _thbToken.deploy()
-await thbToken.mint(100000)
-
-const _ticket = await ethers.getContractFactory('ChokchanaTicket')
-const ticket = await _ticket.deploy(true, 1000, 9999)
-const [owner, acc1] = await ethers.getSigners();
-
-await ticket.mint(1234, owner.address);
-await ticket.getNumberOf(1, 1234)
-await ticket.getNumberOf(1, 1235)
-
-const _lottery = await ethers.getContractFactory('ExternalLottery');
-const lottery = await _lottery.deploy(ticket.address, thbToken.address, 80, 0, 0)
-await thbToken.approve(lottery.address, 10000000);
-
-await lottery.setBuyingPeriod(true)
-await lottery.buyTicket(1234);
-*/
